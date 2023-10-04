9b8fb161bfff65f15249d5696d686b7ef5b2c160
Sohee Moon <soheetech@gmail.com>
2023-10-05 03:10:32 +0900

    위치 수정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,8 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // 확장자 .js 유지할 것
 module.exports = function (app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
+    createProxyMiddleware('/api', {
       target: 'http://localhost:8080',
       changeOrigin: true,
     })
