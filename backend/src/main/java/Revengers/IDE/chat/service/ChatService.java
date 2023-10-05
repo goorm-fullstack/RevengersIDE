01dc0139f1df4d6cd3050025efe7eb0fab1e6d77
Memil <50459192+yss1902@users.noreply.github.com>
2023-10-05 16:17:58 +0900

    JPA + Login Check

--- backend/src/main/java/Revengers/IDE/chat/service/ChatService.java
+++ backend/src/main/java/Revengers/IDE/chat/service/ChatService.java
@@ -14,6 +14,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Slf4j
@@ -49,7 +50,7 @@ public class ChatService {
                 chatMessageEntity.setRoomId(chatMessage.getRoomId());
                 chatMessageEntity.setSender(chatMessage.getSender());
                 chatMessageEntity.setMessage(chatMessage.getMessage());
-                chatMessageEntity.setSentAt(chatMessage.getTime());
+                chatMessageEntity.setSentAt(LocalDateTime.now());
 
                 chatMessageRepository.save(chatMessageEntity);
             }
--- frontend/src/Components/Chat/Chat.tsx
+++ frontend/src/Components/Chat/Chat.tsx
@@ -84,8 +84,22 @@ const Chat = () => {
       </ul>
       <div className="writewrapper">
         <div className="tab">
+          {/** 메시지 삭제 버튼 */}
           <button type="button">
-            {/* SVG 삭제 버튼 */}
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+              <rect fill="none" height="256" width="256" />
+              <line fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" x1="216" x2="40" y1="56" y2="56" />
+              <line fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" x1="104" x2="104" y1="104" y2="168" />
+              <line fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" x1="152" x2="152" y1="104" y2="168" />
+              <path d="M200,56V208a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V56" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" />
+              <path
+                  d="M168,56V40a16,16,0,0,0-16-16H104A16,16,0,0,0,88,40V56"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="16"
+              />
+            </svg>
           </button>
         </div>
         <div className="textareaw">
