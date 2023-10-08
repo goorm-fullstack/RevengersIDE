95b510f4c2682a49de9e5e54a77530b13ab8d322
jinhwan Bea <paewhal20@gmail.com>
2023-10-09 03:27:24 +0900

    배포 오류 해결중

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // 확장자 .js 유지할 것
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api', {
-      target: 'http://localhost:8080',
+    createProxyMiddleware('/ideApi', {
+      target: 'http://3.34.230.219:8080',
       changeOrigin: true,
     })
   );
