b1cb867e1fff99134055c511d051921638de7f11
Sohee Moon <soheetech@gmail.com>
2023-10-10 05:40:48 +0900

    로그인 상태에 따른 ui 전환

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as S from './Style';
 import Logo from '../Logo/Logo';
@@ -9,17 +9,28 @@ import Instance from '../../Utils/api/axiosInstance';
 const Header = () => {
   // 테마 전환: App.tsx에서 가져온 값
   const { toggleTheme, isLight } = useContext(ThemeContext);
-  const isLoggedIn = false;
+  let [isLoggedIn, setLogState] = useState(true); // 로그인 유무 체크
+  let [logMemberName, setLogMemberName] = useState(); // 회원명(아이디)
 
+  // 회원 이름(아이디) 가져오기
   Instance.get('/ideApi/api/member/', { withCredentials: true })
     .then((response) => {
-      console.log(response);
+      // console.log(response);
       if (response.status === 200) {
-        console.log('로그인:' + response.data);
+        if (response.data === '') {
+          // 세션 쿠키 없음
+          setLogState(true);
+        } else {
+          // 세션 쿠키 있음
+          // console.log('헤더:' + response.data);
+          setLogMemberName(response.data);
+          setLogState(false);
+        }
       }
     })
     .catch((error) => {
       console.log(error.data);
+      setLogState(true);
     });
 
   return (
@@ -35,13 +46,13 @@ const Header = () => {
       </div>
       <div className="right">
         {isLoggedIn ? (
-          <div className="member">
-            <Link to="/myaccount">홍구름</Link> 님<button type="button">로그아웃</button>
-          </div>
-        ) : (
           <div className="guest">
             <Link to="/login">로그인</Link>
             <Link to="/signup">회원가입</Link>
+          </div>
+        ) : (
+          <div className="member">
+            <Link to="/myaccount">{logMemberName}</Link> 님<button type="button">로그아웃</button>
           </div>
         )}
       </div>
