969dd9daaad733bc080401ea7152908eef825e4d
jinhwan Bea <paewhal20@gmail.com>
2023-10-10 17:51:27 +0900

    8080 추가

--- frontend/src/Components/Chat/Chat.tsx
+++ frontend/src/Components/Chat/Chat.tsx
@@ -10,7 +10,7 @@ interface Message {
 }
 
 const Chat = () => {
-  const wsURL = 'wss://localhost/ideApi/ws/chat';
+  const wsURL = 'wss://localhost:8080/ideApi/ws/chat';
   const ws = useRef<WebSocket | null>(null);
   const [wsConnected, setWsConnected] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
