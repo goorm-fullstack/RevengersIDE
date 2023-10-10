b5d56eb54bc96918d201c2f0f6bf9f71b437c788
Sohee Moon <soheetech@gmail.com>
2023-10-10 17:59:52 +0900

    채팅 검색 ui 디자인 수정

--- frontend/src/Components/Chat/Chat.tsx
+++ frontend/src/Components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as S from './Style';
 
 type MessageType = 'TALK' | 'ENTER';
@@ -15,11 +15,10 @@ const Chat = () => {
   const [wsConnected, setWsConnected] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
   const [inputMessage, setInputMessage] = useState('');
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
   const messagesEndRef = useRef<null | HTMLDivElement>(null); // 스크롤을 최신채팅으로
   const messageRefs = useRef<{ [key: number]: HTMLLIElement | null }>({}); //
   const [highlightText, setHighlightText] = useState<string | null>(null);
-
 
   // 엔터 입력 시 전송, 쉬프트+엔터 시 다음 줄
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -31,7 +30,7 @@ const Chat = () => {
 
   // 검색
   const searchMessage = (searchText: string) => {
-    const foundIndex = messages.findIndex(message => message.message.includes(searchText));
+    const foundIndex = messages.findIndex((message) => message.message.includes(searchText));
 
     if (foundIndex !== -1 && messageRefs.current[foundIndex]) {
       messageRefs.current[foundIndex]?.scrollIntoView({ behavior: 'smooth' });
@@ -54,7 +53,7 @@ const Chat = () => {
     ws.current.onmessage = (message) => {
       const parsedMessage: Message = JSON.parse(message.data);
 
-      if (parsedMessage.sender === "SERVER" && parsedMessage.type === "ENTER") {
+      if (parsedMessage.sender === 'SERVER' && parsedMessage.type === 'ENTER') {
         setUsername(parsedMessage.message);
         return;
       }
@@ -88,31 +87,41 @@ const Chat = () => {
     }
   };
 
+  const [searchwrap, setToggleSearch] = useState(false);
+  const toggleSearch = () => {
+    setToggleSearch((prev) => !prev);
+  };
+
   return (
     <S.Chat>
       <h3>
-        Chats <span>{messages.length}</span>
-        <input type="text" placeholder="Search..." onChange={(e) => searchMessage(e.target.value)} />
+        <p>
+          Chats <span>{messages.length}</span>
+        </p>
+        <button type="button" onClick={toggleSearch} data-isactive={searchwrap} className="searchbtn">
+          <svg id="Layer_1" version="1.1" viewBox="0 0 50 50">
+            <rect fill="none" height="50" width="50" />
+            <circle cx="21" cy="20" fill="none" r="16" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" />
+            <line fill="none" stroke-miterlimit="10" stroke-width="4" x1="32.229" x2="45.5" y1="32.229" y2="45.5" />
+          </svg>
+        </button>
+        <div className="searchwrap" data-isactive={searchwrap}>
+          <input type="text" placeholder="Search..." onChange={(e) => searchMessage(e.target.value)} />
+        </div>
       </h3>
       <ul className="messagew">
         {messages.map((message, index) => (
-            <li
-                ref={(el) => messageRefs.current[index] = el}
-                key={index}
-                className={message.type === 'ENTER' ? 'enter' : ''}
-            >
-              <strong>{message.sender}: </strong>
-              <span dangerouslySetInnerHTML={{
-                __html: highlightText
-                    ? message.message.replace(new RegExp(`(${highlightText})`, 'gi'), '<mark>$1</mark>')
-                    : message.message
-              }} />
-            </li>
+          <li ref={(el) => (messageRefs.current[index] = el)} key={index} className={message.type === 'ENTER' ? 'enter' : ''}>
+            <strong>{message.sender}: </strong>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: highlightText ? message.message.replace(new RegExp(`(${highlightText})`, 'gi'), '<mark>$1</mark>') : message.message,
+              }}
+            />
+          </li>
         ))}
         <div ref={messagesEndRef}></div>
       </ul>
-
-
       <div className="writewrapper">
         <div className="tab">
           {/** 메시지 삭제 버튼 */}
@@ -124,21 +133,17 @@ const Chat = () => {
               <line fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" x1="152" x2="152" y1="104" y2="168" />
               <path d="M200,56V208a8,8,0,0,1-8,8H64a8,8,0,0,1-8-8V56" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16" />
               <path
-                  d="M168,56V40a16,16,0,0,0-16-16H104A16,16,0,0,0,88,40V56"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="16"
+                d="M168,56V40a16,16,0,0,0-16-16H104A16,16,0,0,0,88,40V56"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
               />
             </svg>
           </button>
         </div>
         <div className="textareaw">
-          <textarea
-              value={inputMessage}
-              onChange={(e) => setInputMessage(e.target.value)}
-              onKeyDown={handleKeyDown}
-          ></textarea>
+          <textarea value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} onKeyDown={handleKeyDown}></textarea>
         </div>
         <button onClick={sendMessage}>Send</button>
       </div>
