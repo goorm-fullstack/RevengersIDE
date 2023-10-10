6ad609be206810e1eb1b8a4ee750b476afb3d097
jinhwan Bea <paewhal20@gmail.com>
2023-10-10 13:18:08 +0900

    웹소켓 끊김 현상 해결 중

--- frontend/src/Components/Chat/Chat.tsx
+++ frontend/src/Components/Chat/Chat.tsx
@@ -66,6 +66,7 @@ const Chat = () => {
     ws.current.onclose = () => {
       console.log('채팅(웹소켓) 연결 해제합니다.');
       setWsConnected(false);
+      ws.current = null;
       setTimeout(() => {
         console.log('재연결 시도...');
         connectWebSoket();
