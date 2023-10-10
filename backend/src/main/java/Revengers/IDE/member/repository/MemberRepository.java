12017052516c131e1e7ed9737739efcb0fb130c1
LEE-Donggyu <ehdrb141@naver.com>
2023-10-10 12:57:24 +0900

    BE : 회원 정보 삭제 구현

--- backend/src/main/java/Revengers/IDE/member/repository/MemberRepository.java
+++ backend/src/main/java/Revengers/IDE/member/repository/MemberRepository.java
@@ -26,4 +26,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByCreateMemberDateBetween(LocalDateTime startOfDay, LocalDateTime endOfDay);
 
+    void deleteByMemberId(String memberId);
+
 }
